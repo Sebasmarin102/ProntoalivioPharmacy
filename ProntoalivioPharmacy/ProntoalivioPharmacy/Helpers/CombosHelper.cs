026bf70b90abf5ca252d4ce208b7c8e3edecfa4f
Sebastian Posada Marín <sebasmarin1099@gmail.com>
2022-05-19 16:01:11 -0500

    Create combos helper

---
+++ ProntoalivioPharmacy/ProntoalivioPharmacy/Helpers/CombosHelper.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ProntoalivioPharmacy.Data;
+
+namespace ProntoalivioPharmacy.Helpers
+{
+    public class CombosHelper : ICombosHelper
+    {
+        private readonly DataContext _context;
+
+        public CombosHelper(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<SelectListItem>> GetComboCitiesAsync()
+        {
+            List<SelectListItem> list = await _context.Cities.Select(c => new SelectListItem
+            {
+                Text = c.Name,
+                Value = c.Id.ToString(),
+            })
+                .OrderBy(m => m.Text)
+                .ToListAsync();
+
+            list.Insert(0, new SelectListItem { Text = "[Seleccione un tipo de Medicina...]", Value = "0" });
+            return list;
+        }
+
+        public async Task<IEnumerable<SelectListItem>> GetComboMedicineTypesAsync()
+        {
+            List<SelectListItem> list = await _context.MedicineTypes.Select(m => new SelectListItem
+            {
+                Text = m.Name,
+                Value = m.Id.ToString(),
+            })
+                .OrderBy(m => m.Text)
+                .ToListAsync();
+
+            list.Insert(0, new SelectListItem { Text = "[Seleccione un tipo de Medicina...]", Value = "0" });
+            return list;
+        }
+    }
+}
---
+++ ProntoalivioPharmacy/ProntoalivioPharmacy/Helpers/ICombosHelper.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ProntoalivioPharmacy.Helpers
+{
+    public interface ICombosHelper
+    {
+        Task<IEnumerable<SelectListItem>> GetComboMedicineTypesAsync();
+
+        Task<IEnumerable<SelectListItem>> GetComboCitiesAsync();
+    }
+}
